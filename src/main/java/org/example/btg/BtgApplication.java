1d8b65d40fb267c67584d32642a61599a1235be6
kopz <gustavoeduardo438@gmail.com>
2024-07-27 17:44:54 -0300

    first commit

---
+++ src/main/java/org/example/btg/BtgApplication.java
@@ -0,0 +1,13 @@
+package org.example.btg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/btg/BtgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.btg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
