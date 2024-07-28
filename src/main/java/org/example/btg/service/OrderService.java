eeebbe9e30cd5b4a1e8ecc918ff3b23f476b90ec
kopz <gustavoeduardo438@gmail.com>
2024-07-27 21:46:18 -0300

    feat(controller): add total products price

--- src/main/java/org/example/btg/service/OrderService.java
+++ src/main/java/org/example/btg/service/OrderService.java
@@ -1,5 +1,6 @@
 package org.example.btg.service;
 
+import org.bson.Document;
 import org.example.btg.controller.dto.OrderResponse;
 import org.example.btg.entity.OrderEntity;
 import org.example.btg.entity.OrderItem;
@@ -7,17 +8,23 @@ import org.example.btg.listener.dto.OrderCreatedEvent;
 import org.example.btg.repository.OrderRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
   private final OrderRepository orderRepository;
+  private final MongoTemplate mongoTemplate;
 
-  public OrderService (OrderRepository orderRepository) {
+  public OrderService (OrderRepository orderRepository , MongoTemplate mongoTemplate) {
     this.orderRepository = orderRepository;
+    this.mongoTemplate = mongoTemplate;
   }
 
   private static List<OrderItem> getOrderItems (OrderCreatedEvent event) {
@@ -36,8 +43,18 @@ public class OrderService {
       .orElse(BigDecimal.ZERO);
   }
 
-  public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest){
-    var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+  public BigDecimal findTotalOnOrdersByCustomerId (Long customerId) {
+    var aggregations = newAggregation(match(Criteria.where("customerId").is(customerId)),
+      group().sum("total").as("total")
+    );
+
+    var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+    return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
+  }
+
+  public Page<OrderResponse> findAllByCustomerId (Long customerId , PageRequest pageRequest) {
+    var orders = orderRepository.findAllByCustomerId(customerId , pageRequest);
     return orders.map(OrderResponse::fromEntity);
   }
 
